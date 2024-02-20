c10eded0f8159ec42d71c4084d8b2176fda53800
bjmickens <128921000+bjmickens@users.noreply.github.com>
2024-02-19 22:03:05 -0600

    Add files via upload

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,8 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Watch out world, I'm learning Java! Yeehaw");
+    }
+}
